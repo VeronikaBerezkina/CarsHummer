09b03771f64763392d1d7c2a3a2979d03f6557c6
Veronika <veronikaberezkina18@gmail.com>
2022-08-07 18:13:50 +0300

    commit generate  numbers with js

--- scripts.js
+++ scripts.js
@@ -6,3 +6,54 @@ function showNav() {
     nav.classList.add('active');
   }
 }
+
+/**
+ *
+ *
+ *
+ */
+
+const nums = document.querySelector('.numbers');
+
+function generateDiv(obj) {
+  //
+  const div = document.createElement('div');
+  div.classList.add('number');
+
+  //
+  const h4 = document.createElement('h4');
+  h4.append(obj.h4);
+  //
+  const p = document.createElement('p');
+  p.append(obj.p);
+
+  //
+  div.append(h4, p);
+  return div;
+}
+
+const array = [
+  {
+    h4: 50,
+    p: 'Салонов',
+  },
+  {
+    h4: 10,
+    p: 'Партнёров',
+  },
+  {
+    h4: '10+',
+    p: 'Звонков в день',
+  },
+  {
+    h4: 50,
+    p: 'Тест-драйвов',
+  },
+];
+/**
+ *
+ */
+
+array.forEach(function (obj) {
+  nums.append(generateDiv(obj));
+});
